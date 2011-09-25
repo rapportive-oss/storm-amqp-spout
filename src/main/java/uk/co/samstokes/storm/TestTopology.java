102c79362d4dc45e9881b97c9233d8fa3f43ba68
Sam Stokes <me@samstokes.co.uk>
2011-09-25 03:30:43 -0700

    Add a main to test the spout.

---
+++ src/main/java/uk/co/samstokes/storm/TestTopology.java
@@ -0,0 +1,41 @@
+package uk.co.samstokes.storm;
+
+import backtype.storm.Config;
+
+import backtype.storm.LocalCluster;
+
+import backtype.storm.topology.TopologyBuilder;
+
+import uk.co.samstokes.storm.spout.AMQPSpout;
+
+public class TestTopology {
+    public static final String TEST_TOPOLOGY_NAME = "amqpTest";
+
+    public static void main (String[] args) {
+        final TopologyBuilder builder = new TopologyBuilder();
+
+        builder.setSpout(1, new AMQPSpout(
+              args[0],
+              Integer.parseInt(args[1]),
+              args[2],
+              args[3],
+              args[4],
+              args[5],
+              args[6]
+              ));
+
+        final Config config = new Config();
+        config.setDebug(true);
+
+        final LocalCluster cluster = new LocalCluster();
+        cluster.submitTopology(TEST_TOPOLOGY_NAME, config, builder.createTopology());
+
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                cluster.killTopology(TEST_TOPOLOGY_NAME);
+                cluster.shutdown();
+            }
+        });
+    }
+}
